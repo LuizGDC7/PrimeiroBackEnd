870edfc89fd48ea86ccd86efd3ec498e70f29f15
LuizGDC7 <luizgdc7@gmail.com>
2024-08-20 16:39:59 -0300

    Adição do YUP para validação do conteúdo do BODY

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,15 +1,44 @@
 import { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import * as yup from 'yup';
 
 // Request e Response são do express
+
+//INTERFACES 
+
 interface Icidade{
     nome:string;
+    // nomes?: string; Exemplo, caso nome não fosse obrigatorio 
 }
 
-export const create = (req: Request<{}, {}, Icidade>, res: Response) => {
+//BODY VALIDATION
+
+const bodyValidation: yup.Schema<Icidade> = yup.object().shape({
+    nome: yup.string().required().min(3),
+});
+
+export const create = async (req: Request<{}, {}, Icidade>, res: Response) => {
 
     const data:Icidade = req.body;
+    let validatedData: Icidade | undefined = undefined;
+/*
+    if(data.nome === undefined){
+        return res.status(StatusCodes.BAD_REQUEST).send("Informe o nome corretamente");
+    }
+*/
+    try{
+        validatedData = await bodyValidation.validate(req.body);
+    }catch(error){
+        const yupError = error as yup.ValidationError;
+        return res.json({
+            errors:{
+                default: yupError.message,
+            }
+        });
+    }
 
-    console.log(data);
+
+    //console.log(data);
 
     return res.send('Create!');
 };
